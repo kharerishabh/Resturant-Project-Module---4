cb0274b949ce28ab7b1ed8203e708c01fbab807f
kharerishabh <rishabkhare41@gmail.com>
2023-01-17 21:52:51 +0530

    Resturant Project Task - 6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layouts/Header";
 import Meals from "./components/Meals/Meals";
 
 const  App = () =>{
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () =>{
+    setCartIsShown(true)
+  }
+  const hideCartHandler = () =>{
+    setCartIsShown(false)
+  }
+
   return (
     <React.Fragment>
-      <Cart/>
-      <Header/>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
       <Meals/>
       </main>
